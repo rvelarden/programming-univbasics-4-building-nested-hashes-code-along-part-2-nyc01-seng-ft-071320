a4879495b658b4a584e7c77ad57a838f23fa9988
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-14 22:08:56 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -32,7 +32,6 @@ end
 
 def add_characters
    epic_tragedy = {
-     
       :montague => {
          :patriarch => {name: "Lord Montague", age: "53"},
          :matriarch => {name: "Lady Montague", age: "54"},
